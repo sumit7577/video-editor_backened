4cfe28c6f26962574f3649f3bbafd3f203e6bb9b
sumit7577 <asmitkumarr082@gmail.com>
2021-08-29 13:25:34 +0530

    video edit fix

--- price_promotion/views.py
+++ price_promotion/views.py
@@ -1,4 +1,3 @@
-from os import write
 from django.http import JsonResponse,HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 from rest_framework.decorators import api_view
@@ -32,13 +31,13 @@ def user_logout(request):
             Token.objects.filter(user_id=user_id).delete()
             logout(request)
             request.session["video"] = ""
-            return JsonResponse({"status":"success","response":f"{username} logged out"})
+            return JsonResponse({"status":"success","message":f"{username} logged out"})
         except: 
-            return JsonResponse({"status":"failure","response":f"{username} already logged out"},status=403)
+            return JsonResponse({"status":"failed","message":f"{username} already logged out"},status=403)
     except User.DoesNotExist:
-        return JsonResponse({"status":"failure","response":f"User does not exist"},status=403)
+        return JsonResponse({"status":"failed","message":f";{username} User does not exist"},status=403)
     except Exception as e:
-        return JsonResponse({"status":"failure","response":f"User logout failed..{e}"},status=500)
+        return JsonResponse({"status":"failed","message":f"User logout failed..{e}"},status=500)
 
 
 
@@ -53,13 +52,13 @@ def get_token(request):
         if user is None:
             return JsonResponse({'status':'failed', 
                                 'token': None, 
-                                'response': 'Please provide correct username and password'},status=403)
+                                'message': 'Please provide correct username and password'},status=403)
         else:
             Token.objects.get_or_create(user=user)
             token = Token.objects.get(user=user)
             return JsonResponse({"status":"success","token":token.key})
     except Exception as e:
-        return JsonResponse({"status":"failure", "response": f"{e}"},status=500)
+        return JsonResponse({"status":"failed", "message": f"{e}"},status=500)
 
 
 
@@ -88,6 +87,15 @@ def create_price_tag(icons,tags,rotate,request):
     tagIMage = tags[0]["image"]
     iconName = base(iconImage)
     tagName = base(tagIMage)
+    if len(icons) >1:
+        iconImage1 = icons[1]["image"]
+        iconLocation1 = icons[1]["location"]
+        iconName1 = base(iconImage1)
+
+    if len(tags) >1:
+        tagImage1 = tags[1]["image"]
+        tagLocation1 = tags[1]["location"]
+        tagName1 = base(tagImage1)
 
     videoFile = os.path.join(settings.BASE_DIR,request.session["video"])
     test = os.path.join(settings.BASE_DIR,uploaded)
@@ -96,22 +104,38 @@ def create_price_tag(icons,tags,rotate,request):
         video = VideoFileClip(test,audio=True).rotate(90)
     else:
         video = VideoFileClip(test,audio=True)
+
     try:
         iconLogo = ImageClip(iconName[0][0]).resize(height=100).margin(top=10,bottom=10,left=10,right=10, opacity=0).set_pos((iconLocation,"bottom"))
     except Exception as re:
         print(f'right tag error {re}')
+
     try:
-        tagLogo = ImageClip(tagName[0][0]).resize(height=100).set_pos((tagLocation,"bottom")).margin(top=10,bottom=10,left=10,right=10, opacity=0)
+        iconLogo1 = ImageClip(iconName1[0][0]).resize(height=100).margin(top=10,bottom=10,left=10,right=10, opacity=0).set_pos((iconLocation1,"bottom"))
+    except Exception as re:
+        print(f'right tag error {re}')
+
+    try:
+        tagLogo = ImageClip(tagName[0][0]).resize(height=100).margin(top=10,bottom=10,left=10,right=10, opacity=0).set_pos((tagLocation,"top"))
+    except Exception as re:
+        print(f'right tag error {re}')
+
+    try:
+        tagLogo1 = ImageClip(tagName1[0][0]).resize(height=100).set_pos((tagLocation1,"top")).margin(top=10,bottom=10,left=10,right=10, opacity=0)
     except Exception as le:
         print(f'left tag error {le}')
 
-    final = CompositeVideoClip([video,iconLogo,tagLogo])
+    final = CompositeVideoClip([video,iconLogo,iconLogo1,tagLogo,tagLogo1])
     final.duration = video.reader.duration
     final.write_videofile("outputVideo.mp4", audio = True,threads=7)
     final.close()
     video.close()
     os.remove(iconName[0][0])
     os.remove(tagName[0][0])
+    if os.path.exists(iconName1[0][0]):
+        os.remove(iconName1[0][0])
+    if os.path.exists(tagName1[0][0]):
+        os.remove(tagName1[0][0])
 
 # def mongo_conn():
 #     try:
@@ -130,10 +154,10 @@ def upload(request):
     try:
         token = request.headers["Authorization"][7:]
     except:
-        return JsonResponse({"status":"Error","message":"Please insert Token in header"},status=403)
+        return JsonResponse({"status":"failed","message":"Please insert Token in header"},status=403)
     data = Token.objects.filter(key=token)
     if not data:
-        return JsonResponse({"status":"Error","message":"Please input valid Token"},status=401)
+        return JsonResponse({"status":"failed","message":"Please input valid Token"},status=401)
     #username = data[0].user
     files = request_body["path"]
     decodedFile = base(files)
@@ -167,16 +191,16 @@ def download(request):
         rotate = request_body["rotate"]
         
     except:
-        return JsonResponse({"status":"failure","message":"Please provide tags and icons"},status=403)
+        return JsonResponse({"status":"failed","message":"Please provide tags and icons"},status=403)
 
     try:
         token = request.headers["Authorization"][7:]
         userId = User.objects.filter(auth_token__key= token)
         if(not userId):
-            return JsonResponse({"status":"Error","message":"Please input valid Token"},status=401)
+            return JsonResponse({"status":"failed","message":"Please input valid Token"},status=401)
         #username = userId[0].id
     except:
-        return JsonResponse({"status":"Error","message":"Please insert Token in header"},status=403)
+        return JsonResponse({"status":"failed","message":"Please insert Token in header"},status=403)
     
     create_price_tag(icons,tags,rotate,request)
 
@@ -193,6 +217,6 @@ def download(request):
         try:
             return response
         except:
-            return JsonResponse({"status":"failed"},status=500)
+            return JsonResponse({"status":"failed","message":"something error happened"},status=500)
 
     
