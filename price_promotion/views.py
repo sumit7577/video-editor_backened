1a877de84bec9eb2a6f20512005a243d70bcbe86
sumit7577 <asmitkumarr082@gmail.com>
2021-10-01 19:40:19 +0530

    some more fix after merge videos

--- price_promotion/views.py
+++ price_promotion/views.py
@@ -288,7 +288,7 @@ def upload(request):
         videoList = []
         final_duration = 0
         for i in uploaded:
-            video = VideoFileClip(i,audio=True,target_resolution=(200,200))
+            video = VideoFileClip(i,audio=True,target_resolution=(480,480))
             video.duration = video.reader.duration
             videoList.append(video)
             final_duration += video.duration
